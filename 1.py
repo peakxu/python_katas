3b46a8cb3143dea4cfd84085165f4307576cb9b0
Peak Xu <peak.xu@gmail.com>
2014-06-14 14:30:58 -0400

    Python kata 1

---
+++ 1.py
@@ -0,0 +1,6 @@
+enc = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+intab = '{|' + string.ascii_lowercase
+outtab = string.ascii_lowercase + 'ab'
+trantab = string.maketrans(intab, outtab)
+enc.translate(trantab)
+"i hope you didnt translate it by hand. thats what computers are for. doing it in by hand is inefficient and that's why this text is so long. using string.maketrans() is recommended. now apply on the url."
